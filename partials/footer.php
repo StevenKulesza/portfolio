1a002d781b4cd542ddc2e1121e277689e129f5c8
Steven Kulesza <stevenn.kulesza@gmail.com>
2016-10-06 16:03:29 +0000

    rm bs js

--- partials/footer.php
+++ partials/footer.php
@@ -25,7 +25,6 @@
         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jQuery-viewport-checker/1.8.7/jquery.viewportchecker.min.js"></script>
         <script type="text/javascript" src="js/sk.js"></script>
-<!--         <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script> -->
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
         
 <!--
